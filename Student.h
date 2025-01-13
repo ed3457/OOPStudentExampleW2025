7d59c2bd8ae122f3ea5f0dc4a31804bfd0ddc6b5
Kamel R <kamel.r@wayne.edu>
2025-01-13 18:32:02 -0500

    finished with the header file for the student class

--- Student.h
+++ Student.h
@@ -1,5 +1,28 @@
 #pragma once
+#include <string>
+using namespace std; 
 class Student
 {
+	// Data
+private: 
+	string name;
+	string id;	
+	string major;
+	// Mutators (set/get functions) 
+public: 
+	void setName(string n); 
+	string getName();
+
+	void setID(string i);
+	string getID(); 
+
+	void setMajor(string m);
+	string getMajor();
+
+
+	// define functions 
+	void printStudentInfo();
+
+
 };
 
