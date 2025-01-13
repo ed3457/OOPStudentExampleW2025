bfc05081f8aa60784c26770b36bf61e8aa4a7960
Kamel R <kamel.r@wayne.edu>
2025-01-13 18:59:35 -0500

    added some objects to the main

--- OOPStudentExampleW2025.cpp
+++ OOPStudentExampleW2025.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std; 
+#include "Student.h"
 int main()
 {
-    cout << "Hello World!" << endl;
+	Student student1;
+	Student student2;
+
+	student1.setID("12345");
+	student1.setName("Jack");
+	student1.setMajor("Math");
+
+	student1.printStudentInfo();
+
 }
 
